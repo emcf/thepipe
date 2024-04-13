7ebd512a540dd49d979d527bd24721957b702bb3
Emmett McFaralne <staminacode@gmail.com>
2024-04-13 16:08:48 -0400

    set gitignore for pypi, setup fix

--- setup.py
+++ setup.py
@@ -1,3 +1,21 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup()
+setup(
+    name='thepipe_api',
+    version='0.1.3',
+    author='Emmett McFarlane',
+    author_email='emmett@thepi.pe',
+    description='Automate information extraction for multimodal LLMs.',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/emcf/thepipe',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=open('requirements.txt').read().splitlines(),
+    include_package_data=True
+)
