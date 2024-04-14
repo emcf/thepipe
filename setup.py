8b034d3367f37385cebe97ffe17bbb2a6a4c0576
Emmett McFaralne <staminacode@gmail.com>
2024-04-14 17:15:42 -0400

    chose appropriate new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='thepipe_api',
-    version='0.1.5',
+    version='0.1.6',
     author='Emmett McFarlane',
     author_email='emmett@thepi.pe',
     description='Automate information extraction for multimodal LLMs.',
