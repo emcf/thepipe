00346944b56e2f9d447a541776672d6b79f55c08
Emmett McFaralne <staminacode@gmail.com>
2024-07-11 13:17:23 -0400

    now passing local to scrape_directory

--- thepipe/__init__.py
+++ thepipe/__init__.py
@@ -6,11 +6,11 @@ def main() -> None:
     args = parse_arguments()
     chunks = None
     if args.source.startswith("http"):
-        chunks = scrape_url(args.source, local=args.local, verbose=args.verbose)
+        chunks = scrape_url(args.source, local=args.local, verbose=args.verbose, local=args.local)
     elif os.path.isdir(args.source):
-        chunks = scrape_directory(args.source, verbose=args.verbose)
+        chunks = scrape_directory(args.source, verbose=args.verbose, local=args.local)
     else:
-        chunks = scrape_file(args.source, local=args.local, verbose=args.verbose)
+        chunks = scrape_file(args.source, local=args.local, verbose=args.verbose, local=args.local)
     save_outputs(chunks=chunks, verbose=args.verbose, text_only=args.text_only)
 
 if __name__ == "__main__":
