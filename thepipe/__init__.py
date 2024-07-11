ff81c3e6b7be175ee80d6b8c4586dd32f26876c9
Emmett McFaralne <staminacode@gmail.com>
2024-07-11 13:25:34 -0400

    Passing all available args to command line version

--- thepipe/__init__.py
+++ thepipe/__init__.py
@@ -6,11 +6,11 @@ def main() -> None:
     args = parse_arguments()
     chunks = None
     if args.source.startswith("http"):
-        chunks = scrape_url(args.source, local=args.local, verbose=args.verbose, local=args.local)
+        chunks = scrape_url(args.source, text_only=args.text_only, ai_extraction=args.ai_extraction, verbose=args.verbose, local=args.local)
     elif os.path.isdir(args.source):
-        chunks = scrape_directory(args.source, verbose=args.verbose, local=args.local)
+        chunks = scrape_directory(args.source, include_regex=args.include_regex text_only=args.text_only, ai_extraction=args.ai_extraction, verbose=args.verbose, local=args.local)
     else:
-        chunks = scrape_file(args.source, local=args.local, verbose=args.verbose, local=args.local)
+        chunks = scrape_file(args.source, text_only=args.text_only, ai_extraction=args.ai_extraction, verbose=args.verbose, local=args.local)
     save_outputs(chunks=chunks, verbose=args.verbose, text_only=args.text_only)
 
 if __name__ == "__main__":
