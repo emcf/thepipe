afc35e5fca6832c80511bea51a27f55ed812c3a4
Emmett McFaralne <staminacode@gmail.com>
2024-07-26 01:24:00 -0400

    fixed path testing in scraper

--- tests/test_core.py
+++ tests/test_core.py
@@ -35,7 +35,7 @@ class test_core(unittest.TestCase):
             self.assertEqual(type(message), dict)
             self.assertIn('role', message)
             self.assertIn('content', message)
-            self.assertIn('example.md', message['content'])
+            self.assertIn('example.md', message['content'][0]['text'])
 
     def test_save_outputs(self):
         chunks = scraper.scrape_plaintext(file_path=self.files_directory+"/example.txt")
